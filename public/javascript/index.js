32239b1e0c8b25b2f62f365873c45d88c4dbdb3c
Siri Persson <siripersson@Siris-MacBook-Pro.local>
2018-02-07 17:30:58 +0100

    Cleaning of code

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -17,11 +17,19 @@ const take_photo_btn=document.getElementById('take-photo')
 const delete_photo_btn=document.getElementById('delete-photo')
 const save_photo_btn=document.getElementById('save-photo')
 
+//Function to take snapshot
+const takeSnapShot = () => {
+	const width= document.getElementById('camera-stream').width; // get width of videostream
+	const height= document.getElementById('camera-stream').height;
+    const context = hidden_canvas.getContext("2d") // set context
+    context.drawImage(video,0,0,width,height) // draw image
+    return hidden_canvas.toDataURL("image/png") // return image
+}
+
 // Eventlistener for take photo button
 take_photo_btn.addEventListener('click', (e) =>{
-	console.log("Clicked on take-photo")
-	const snap= takeSnapShot() //anropa snapchot funktionen, den returnerar en bild
-	document.getElementById("snap").style.display = "inline";
+	const snap= takeSnapShot() //call snapchot function
+	document.getElementById("snap").style.display = "inline"; // place the snapshot beside the video
 	const image_tag= document.getElementById('snap') // get image tag
 	image_tag.setAttribute("src", snap) // set attribute src
 
@@ -33,44 +41,22 @@ take_photo_btn.addEventListener('click', (e) =>{
 
 	// Save photo
 	const save_tag= document.getElementById('save-photo')
-	console.log('Get save button: '+save_tag)
 	save_tag.setAttribute("href", snap) // set attribute href
-	console.log('Set attribute href for save photo: ' +save_tag.setAttribute("href", snap) )
 	e.preventDefault(); 
 })
-
-// Eventlistener for save button
-save_photo_btn.addEventListener('click', (e) =>{
-	console.log("save photo")
- }) 
 
 // Eventlistener for delete button
 delete_photo_btn.addEventListener('click', (e) =>{
 	const image_tag2= document.getElementById('snap') // get image tag
 	image_tag2.setAttribute("src",'') // set attribute src
-	document.getElementById("snap").style.display = "none";
+	document.getElementById("snap").style.display = "none"; // set display to none
 
-	// Save photo
+	// Delete saved photo
 	const save_tag2= document.getElementById('save-photo')
-	save_tag2.setAttribute("href", '') // set attribute href
-	console.log("Delete photo")
+	save_tag2.setAttribute("href", '') // set attribute href 
 	e.preventDefault(); 
 }) 
 
-
-//Function
-const takeSnapShot = () => {
-	const width= document.getElementById('camera-stream').width; // get width of videostream
-	const height= document.getElementById('camera-stream').height;
-	console.log("Width: " + width)
-	console.log("Height: " + height)
-
-	 // Grab elements, create settings, etc.
-    const context = hidden_canvas.getContext("2d")
-    context.drawImage(video,0,0,width,height)
-    return hidden_canvas.toDataURL("image/png") // returnerar en bild
-
-}
 
 
 
