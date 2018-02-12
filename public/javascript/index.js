7b9c05e78877b3e93549a03417abf122da7804cf
Siri Persson <siripersson@Siris-MacBook-Pro.local>
2018-02-12 17:30:44 +0100

    Removed redundant code

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -19,8 +19,8 @@ const save_photo_btn=document.getElementById('save-photo')
 
 //Function to take snapshot
 const takeSnapShot = () => {
-	const width= document.getElementById('video').width; // get width of videostream
-	const height= document.getElementById('video').height;
+	const width= video.width; // get width of videostream
+	const height= video.height;
     const context = canvas.getContext("2d") // set context
     context.drawImage(video,0,0,width,height) // draw image
     return canvas.toDataURL("image/png") // return image
@@ -29,9 +29,9 @@ const takeSnapShot = () => {
 // Eventlistener for take photo button
 take_photo_btn.addEventListener('click', (e) =>{
 	const image= takeSnapShot() //call snapchot function
-	document.getElementById("image").style.display = "inline"; // place the snapshot beside the video
-	const image_tag= document.getElementById('image') // get image tag
-	image_tag.setAttribute("src", image) // set attribute src
+	const image_element=document.getElementById("image");
+	image_element.style.display = "inline"; // place the snapshot beside the video
+	image_element.setAttribute("src", image) // set attribute src
 
 	// To create a flash
 	document.getElementById("flash").style.display = "inline";
@@ -40,20 +40,18 @@ take_photo_btn.addEventListener('click', (e) =>{
 	}, 200);
 
 	// Save photo
-	const save_tag= document.getElementById('save-photo')
-	save_tag.setAttribute("href", image) // set attribute href
+	save_photo_btn.setAttribute("href", image) // set attribute href
 	e.preventDefault(); 
 })
 
 // Eventlistener for delete button
 delete_photo_btn.addEventListener('click', (e) =>{
-	const image_tag2= document.getElementById('image') // get image tag
-	image_tag2.setAttribute("src",'') // set attribute src
-	document.getElementById("image").style.display = "none"; // set display to none
+	const image_element=document.getElementById("image");
+	image_element.setAttribute("src",'') // set attribute src
+	image_element.style.display = "none"; // set display to none
 
 	// Delete saved photo
-	const save_tag2= document.getElementById('save-photo')
-	save_tag2.setAttribute("href", '') // set attribute href 
+	save_photo_btn.setAttribute("href", '') // set attribute href 
 	e.preventDefault(); 
 }) 
 
