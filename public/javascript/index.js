ae0ce1d8afe22c070903d10283ac45e729668231
Siri Persson <siripersson@Siris-MacBook-Pro.local>
2018-02-13 13:23:28 +0100

    Cleaning code and making variables names clearer

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,6 +1,14 @@
 
+// Get html elements
 const video= document.getElementById('video')// Get video stream
 const canvas = document.getElementById('canvas'); // Get canvas
+const image=document.getElementById('image');
+
+// Get buttons
+const takePhotoButton=document.getElementById('take-photo')
+const deleteButton=document.getElementById('delete-photo')
+const saveButton=document.getElementById('save-photo')
+
 
 // Fetch video stream
 navigator.mediaDevices.getUserMedia({ video: true })
@@ -12,26 +20,20 @@ navigator.mediaDevices.getUserMedia({ video: true })
 		console.error(err);
 	});
 
-// Fetch the buttons
-const take_photo_btn=document.getElementById('take-photo')
-const delete_photo_btn=document.getElementById('delete-photo')
-const save_photo_btn=document.getElementById('save-photo')
-
-//Function to take snapshot
+//Function to take the snapshot
 const takeSnapShot = () => {
 	const width= video.width; // get width of videostream
-	const height= video.height;
+	const height= video.height; // get height
     const context = canvas.getContext("2d") // set context
     context.drawImage(video,0,0,width,height) // draw image
     return canvas.toDataURL("image/png") // return image
 }
 
 // Eventlistener for take photo button
-take_photo_btn.addEventListener('click', (e) =>{
-	const image= takeSnapShot() //call snapchot function
-	const image_element=document.getElementById("image");
-	image_element.style.display = "inline"; // place the snapshot beside the video
-	image_element.setAttribute("src", image) // set attribute src
+takePhotoButton.addEventListener('click', (e) =>{
+	const picture= takeSnapShot() //call snapchot function
+	image.style.display = "inline"; // place the snapshot beside the video
+	image.setAttribute("src", picture) // set attribute src
 
 	// To create a flash
 	document.getElementById("flash").style.display = "inline";
@@ -40,18 +42,17 @@ take_photo_btn.addEventListener('click', (e) =>{
 	}, 200);
 
 	// Save photo
-	save_photo_btn.setAttribute("href", image) // set attribute href
+	saveButton.setAttribute("href", picture) // set attribute href
 	e.preventDefault(); 
 })
 
 // Eventlistener for delete button
-delete_photo_btn.addEventListener('click', (e) =>{
-	const image_element=document.getElementById("image");
-	image_element.setAttribute("src",'') // set attribute src
-	image_element.style.display = "none"; // set display to none
+deleteButton.addEventListener('click', (e) =>{
+	image.setAttribute("src",'') // set attribute src
+	image.style.display = "none"; // set display to none
 
 	// Delete saved photo
-	save_photo_btn.setAttribute("href", '') // set attribute href 
+	saveButton.setAttribute("href", '') // set attribute href 
 	e.preventDefault(); 
 }) 
 
